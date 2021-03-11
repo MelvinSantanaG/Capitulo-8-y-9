1c844fb018d3e06d40245a4f6d0a110383b0bd9a
Melvin Ernesto Santana Garcia <melvin_santana@ucne.edu.do>
2021-03-11 10:01:48 -0500

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capitulo_8_y_9
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
